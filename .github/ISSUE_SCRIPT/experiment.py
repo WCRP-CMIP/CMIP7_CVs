1ccf95d98ee1c7b15059381ce5090effcc2faa03
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-03-14 14:28:44 +0000

    development

--- .github/ISSUE_SCRIPT/experiment.py
+++ .github/ISSUE_SCRIPT/experiment.py
@@ -49,7 +49,7 @@ def run(issue,packet):
     git.update_summary(f"### Branch created: {branch}, {os.popen('git branch').read()}")
     
     gb = os.popen('git branch').read()
-    assert gb == branch, 'the branch is not the same (not created) "{gb}" != "{branch}"'
+    assert gb == branch, f'the branch is not the same (not created) "{gb}" != "{branch}"'
     
     
     # activity
